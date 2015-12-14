f351a440e67febce23bf56a58a523f4adab561a4
wbq501 <749937855@qq.com>
2015-12-14 18:25:15 +0800

    viewpager 绑定图片和横线

--- app/src/main/java/com/wbq/mvp/DongHua3Activity.java
+++ app/src/main/java/com/wbq/mvp/DongHua3Activity.java
@@ -4,11 +4,11 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.DisplayMetrics;
-import android.view.Display;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -16,11 +16,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 /**
  * Created by wbq501 on 2015-12-11 16:37.
  * demo
  */
-public class DongHua3Activity extends Activity{
+public class DongHua3Activity extends FragmentActivity {
     AnimatorSet animatorset1,animatorset2,animatorset3;
     TextView ceshi1,ceshi2,ceshi3,henxian;
     ObjectAnimator animleftdown1,animleftdown2,animdownright1,animdownright2,animrightleft1
@@ -31,6 +37,12 @@ public class DongHua3Activity extends Activity{
     int changright = 1;
     int xleft,x,xright,y,downy;
     ImageView imganim;
+    ViewPager pager;
+    private int pagerchange=1;
+    /**
+     * changpagerimg ==0 不切换 1右切换 2左切换
+     */
+    private int changpagerimg = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +52,8 @@ public class DongHua3Activity extends Activity{
         ceshi3 = (TextView) findViewById(R.id.ceshi3);
         henxian = (TextView) findViewById(R.id.henxian);
         imganim = (ImageView) findViewById(R.id.imganim);
+        pager = (ViewPager) findViewById(R.id.pager);
+        setupViewPager(pager);
         ceshi1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,13 +85,108 @@ public class DongHua3Activity extends Activity{
             }
         });
     }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new TabFragment(), "Tab 1");
+        adapter.addFrag(new TabFragment(), "Tab 2");
+        adapter.addFrag(new TabFragment(), "Tab 3");
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(1);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                int changnum = pagerchange-position;
+                switch (changnum){
+                    case 0:
+                        changpagerimg = 0;
+                        break;
+                    case 1:
+                        changpagerimg = 1;
+                        break;
+                    case -1:
+                        changpagerimg = 2;
+                        break;
+                }
+                pagerchange = position;
+                switch (position) {
+                    case 0:
+                        viewpageimgchang();
+                        break;
+                    case 1:
+                        viewpageimgchang();
+                        break;
+                    case 2:
+                        viewpageimgchang();
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private void viewpageimgchang() {
+        switch (changpagerimg){
+            case 0:
+                break;
+            case 1:
+                imganimright();
+                chooseanimright(changright);
+                clickanimright();
+                break;
+            case 2:
+                imganimleft();
+                chooseanimleft(changleft);
+                clickanimleft();
+                break;
+        }
+    }
+
+
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<Fragment>();
+        private final List<String> mFragmentTitleList = new ArrayList<String>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Toast.makeText(DongHua3Activity.this,"2222222",Toast.LENGTH_SHORT).show();
         return super.onTouchEvent(event);
     }
+
+    /**
+     * 图片左，横线右
+     */
     private void imganimleft() {
-        Toast.makeText(DongHua3Activity.this,"111111",Toast.LENGTH_SHORT).show();
         int x1 = imganim.getLeft();
         int imgw = imganim.getRight()-x1;
         DisplayMetrics dm = new DisplayMetrics();
@@ -89,13 +198,39 @@ public class DongHua3Activity extends Activity{
         animatorSet.play(objectAnimator2).after(objectAnimator1);
         animatorSet.setDuration(350);
         animatorSet.start();
-        ObjectAnimator oaxian = ObjectAnimator.ofFloat(henxian,"X",ceshi1.getLeft(),henxian.getLeft());
+        ObjectAnimator oaxian = ObjectAnimator.ofFloat(henxian,"X",henxian.getLeft()-100,henxian.getLeft());
         AnimatorSet asxian = new AnimatorSet();
         asxian.play(oaxian);
         asxian.setDuration(800);
         asxian.start();
     }
 
+    /**
+     * 图片右，横线左
+     */
+    private void imganimright() {
+        int x1 = imganim.getLeft();
+        int imgw = imganim.getRight()-x1;
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int x2 = dm.widthPixels;
+        ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(imganim,"X",x1,x2);
+        ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(imganim,"X",-imgw,x1);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(objectAnimator2).after(objectAnimator1);
+        animatorSet.setDuration(350);
+        animatorSet.start();
+        ObjectAnimator oaxian = ObjectAnimator.ofFloat(henxian,"X",henxian.getLeft()+100,henxian.getLeft());
+        AnimatorSet asxian = new AnimatorSet();
+        asxian.play(oaxian);
+        asxian.setDuration(800);
+        asxian.start();
+    }
+
+    /**
+     * 顺时针动画改变
+     * @param i
+     */
     private void chooseanimleft(int i) {
         changxy();
         switch (i){
@@ -132,6 +267,9 @@ public class DongHua3Activity extends Activity{
         }
     }
 
+    /**
+     * changxy 三个按钮坐标
+     */
     private void changxy() {
         xleft = ceshi1.getLeft();
         x = ceshi2.getLeft();
@@ -140,6 +278,10 @@ public class DongHua3Activity extends Activity{
         downy = ceshi2.getTop();
     }
 
+    /**
+     * 逆时针动画的改变
+     * @param i
+     */
     private void chooseanimright(int i) {
         changxy();
         switch (i){
@@ -175,6 +317,10 @@ public class DongHua3Activity extends Activity{
                 break;
         }
     }
+
+    /**
+     * clickanimleft 顺时针动画
+     */
     private void clickanimleft() {
         animatorset1 = new AnimatorSet();
         animatorset2 = new AnimatorSet();
@@ -214,6 +360,10 @@ public class DongHua3Activity extends Activity{
             }
         });
     }
+
+    /**
+     * clickanimright 逆时针动画
+     */
     private void clickanimright() {
         animatorset1 = new AnimatorSet();
         animatorset2 = new AnimatorSet();

35d789b3c79ad832ed2e0b308730fd09693a457c
wbq501 <749937855@qq.com>
2015-12-16 11:48:28 +0800

    自定义view

--- app/src/main/java/com/wbq/mvp/view/RotateView.java
+++ app/src/main/java/com/wbq/mvp/view/RotateView.java
@@ -2,7 +2,9 @@ package com.wbq.mvp.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 /**
  * Created by wbq501 on 2015-12-16 09:42.
@@ -18,16 +20,49 @@ public class RotateView extends ViewGroup{
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int resWidth = 0;
+        int resHeight = 0;
         /**
-         * 获得此ViewGroup上级容器为其推荐的宽和高，以及计算模式
+         * 根据传入的参数，分别获取测量模式和测量值
          */
+        int width = MeasureSpec.getSize(widthMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+
+        int height = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
-    }
+        /**
+         * 如果宽或者高的测量模式非精确值
+         */
+        if (widthMode != MeasureSpec.EXACTLY
+                || heightMode != MeasureSpec.EXACTLY)
+        {
+            // 主要设置为背景图的高度
+            resWidth = getSuggestedMinimumWidth();
+            // 如果未设置背景图片，则设置为屏幕宽高的默认值
+            resWidth = resWidth == 0 ? getDefaultWidth() : resWidth;
 
+            resHeight = getSuggestedMinimumHeight();
+            // 如果未设置背景图片，则设置为屏幕宽高的默认值
+            resHeight = resHeight == 0 ? getDefaultWidth() : resHeight;
+        } else
+        {
+            // 如果都设置为精确值，则直接取小值；
+            resWidth = resHeight = Math.min(width, height);
+        }
+    }
+    /**
+     * 获得默认该layout的尺寸
+     * @return
+     */
+    private int getDefaultWidth()
+    {
+        WindowManager wm = (WindowManager) getContext().getSystemService(
+                Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return Math.min(outMetrics.widthPixels, outMetrics.heightPixels);
+    }
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         
